4a5eac2ad91eb872573830fdbcebf8506a5b6636
Sy P <yongsa0221@gmail.com>
2023-02-15 17:31:02 +0900

    feat : card 클래스 추가

---
+++ src/main/java/gmbs/model/Card.java
@@ -0,0 +1,33 @@
+package gmbs.model;
+
+import gmbs.model.black_jack_enum.BlackJackValue;
+import gmbs.model.black_jack_enum.CardSuits;
+
+public class Card {
+
+    private final CardSuits cardShape;
+    private final BlackJackValue cardValue;
+    private final String cardName;
+    
+    public Card(final CardSuits shape, final BlackJackValue value) {
+        cardShape = shape;
+        cardValue = value;
+        cardName = shape.getDescription() + value.getDescription();
+    }
+    
+    public int getValue() {
+        return cardValue.value();
+    }
+
+    public int getAlternativeValue() {
+        return cardValue.alternativeValue();
+    }
+
+    public String getName() {
+        return cardName;
+    }
+
+    public CardSuits getShape() {
+        return cardShape;
+    }
+}
