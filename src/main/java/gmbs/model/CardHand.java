3aa51d3c6681bbb4196b128a175b3ebb82a0d101
Sy P <yongsa0221@gmail.com>
2023-02-15 19:48:22 +0900

    feat : 카드 패 관련 CardHand 클래스 추가

---
+++ src/main/java/gmbs/model/CardHand.java
@@ -0,0 +1,27 @@
+package gmbs.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardHand {
+
+    private final List<Card> cards;
+
+    public static CardHand of(List<Card> cards) {
+        return new CardHand(cards);
+    }
+
+    public static CardHand from(CardHand cardHand, Card addCard) {
+        List<Card> addedCards = new ArrayList<>(cardHand.getCards());
+        addedCards.add(addCard);
+        return new CardHand(addedCards);
+    }
+
+    private CardHand(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+}
