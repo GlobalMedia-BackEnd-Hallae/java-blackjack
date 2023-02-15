7d472fc3c0b2144bf44149e8dc12300f47222f6a
Sy P <yongsa0221@gmail.com>
2023-02-16 00:24:43 +0900

    feat : dealer 추가

---
+++ src/main/java/gmbs/model/players/Dealer.java
@@ -0,0 +1,18 @@
+package gmbs.model.players;
+
+import gmbs.model.Card;
+import gmbs.model.CardHand;
+
+public class Dealer implements Player {
+
+    private final CardHand cards;
+
+    public Dealer(CardHand cards) {
+        this.cards = cards;
+    }
+
+    @Override
+    public Dealer hit(Card card) {
+        return new Dealer(CardHand.from(this.cards, card));
+    }
+}
