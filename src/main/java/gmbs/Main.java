892d644bd10b619fd691a5ee1fbcc82444917655
Sy P <yongsa0221@gmail.com>
2023-02-21 04:59:41 +0900

    feat : main 추가

--- src/main/java/gmbs/Main.java
+++ src/main/java/gmbs/Main.java
@@ -1,7 +1,11 @@
 package gmbs;
 
+import gmbs.controller.Controller;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Controller controller = new Controller();
+        controller.playBlackJack();
+        controller.playBlackJack();
     }
 }
