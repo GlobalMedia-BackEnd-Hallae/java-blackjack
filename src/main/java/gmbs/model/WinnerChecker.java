0ef5b23219df59121729d53d98890f02d635bfe7
Sy P <yongsa0221@gmail.com>
2023-02-20 19:06:20 +0900

    feat : 비교 대상 result 기능 추가

--- src/main/java/gmbs/model/WinnerChecker.java
+++ src/main/java/gmbs/model/WinnerChecker.java
@@ -3,19 +3,29 @@ package gmbs.model;
 import gmbs.model.black_jack_enum.Result;
 import gmbs.model.players.Player;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class WinnerChecker {
 
     private static final int BLACK_JACK = 21;
     private final HandCalculator calculator = new HandCalculator();
     private final int targetHandValue;
+    private final Map<Result, Integer> targetResults;
 
     public WinnerChecker(Player comparisonTarget) {
         targetHandValue = calculator.sumHand(comparisonTarget.getCardHand());
+        targetResults = Arrays.stream(Result.values())
+                .collect(Collectors.toMap(Function.identity(), ignored -> 0));
     }
 
-    public Result getResult(Player player) {
-        int userHandValue = calculator.sumHand(player.getCardHand());
-        return getResultByValue(userHandValue);
+    public Result getResult(Player opponent) {
+        int opponentSumHand = calculator.sumHand(opponent.getCardHand());
+        Result opponentResult = getResultByValue(opponentSumHand);
+        addComparisonTargetResults(opponentResult);
+        return opponentResult;
     }
 
     private Result getResultByValue(int userHandValue) {
@@ -31,6 +41,10 @@ public class WinnerChecker {
         return Result.DRAW;
     }
 
+    private boolean isBust(int sumValue) {
+        return sumValue > BLACK_JACK;
+    }
+
     private Result getNoBustResult(int userHandValue) {
         if (targetHandValue > userHandValue) {
             return Result.LOSE;
@@ -41,8 +55,12 @@ public class WinnerChecker {
         return Result.DRAW;
     }
 
-    private boolean isBust(int sumValue) {
-        return sumValue > BLACK_JACK;
+    private void addComparisonTargetResults(Result opponentResult) {
+        Result targetResult = opponentResult.getOpponent();
+        targetResults.replace(targetResult, targetResults.get(targetResult) + 1);
     }
 
+    public Map<Result, Integer> getTargetResults() {
+        return targetResults;
+    }
 }
--- src/test/java/gmbs/model/WinnerCheckerTest.java
+++ src/test/java/gmbs/model/WinnerCheckerTest.java
@@ -6,16 +6,27 @@ import gmbs.model.black_jack_enum.Result;
 import gmbs.model.players.Dealer;
 import gmbs.model.players.User;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class WinnerCheckerTest {
+
+    private static final Card spadeAce = new Card(CardSuits.SPADE, BlackJackValue.ACE);
+    private static final Card clubAce = new Card(CardSuits.CLUB, BlackJackValue.ACE);
+    private static final Card spadeQueen = new Card(CardSuits.SPADE, BlackJackValue.QUEEN);
+    private static final Card spadeSix = new Card(CardSuits.SPADE, BlackJackValue.SIX);
+    private static final CardHand bust = CardHand.of(List.of(spadeQueen, spadeQueen, spadeSix));
+    private static final CardHand blackJack = CardHand.of(List.of(spadeAce, spadeQueen));
+    private static final CardHand sixteen = CardHand.of(List.of(spadeQueen, spadeSix));
+    private static final CardHand twelve = CardHand.of(List.of(spadeAce, clubAce));
 
     @ParameterizedTest
     @DisplayName("생성 시 넘겨주는 player를 기준으로 cardHand를 비교하여 승,무,패를 반환")
@@ -32,14 +43,6 @@ class WinnerCheckerTest {
     }
 
     private static Stream<Arguments> hands() {
-        Card spadeAce = new Card(CardSuits.SPADE, BlackJackValue.ACE);
-        Card clubAce = new Card(CardSuits.CLUB, BlackJackValue.ACE);
-        Card spadeQueen = new Card(CardSuits.SPADE, BlackJackValue.QUEEN);
-        Card spadeSix = new Card(CardSuits.SPADE, BlackJackValue.SIX);
-        CardHand bust = CardHand.of(List.of(spadeQueen, spadeQueen, spadeSix));
-        CardHand blackJack = CardHand.of(List.of(spadeAce, spadeQueen));
-        CardHand sixteen = CardHand.of(List.of(spadeQueen, spadeSix));
-        CardHand twelve = CardHand.of(List.of(spadeAce, clubAce));
         return Stream.of(
                 Arguments.of(blackJack, blackJack, Result.DRAW),
                 Arguments.of(bust, blackJack, Result.WIN),
@@ -49,5 +52,25 @@ class WinnerCheckerTest {
                 Arguments.of(sixteen, twelve, Result.LOSE),
                 Arguments.of(twelve, sixteen, Result.WIN)
         );
+    }
+
+    @Test
+    @DisplayName("target의 상대player에 따른 결과들을 비교하여 반환")
+    void getComparisonTargetResults() {
+        //given
+        Dealer target = new Dealer(sixteen);
+        User winUser = new User("win", blackJack);
+        User loseUser = new User("lose", twelve);
+        User drawUser = new User("draw", sixteen);
+        WinnerChecker checker = new WinnerChecker(target);
+        checker.getResult(winUser);
+        checker.getResult(loseUser);
+        checker.getResult(drawUser);
+        //when
+        Map<Result, Integer> targetResult = checker.getTargetResults();
+        //then
+        assertThat(targetResult).containsEntry(Result.WIN, 1)
+                .containsEntry(Result.LOSE, 1)
+                .containsEntry(Result.DRAW, 1);
     }
 }
