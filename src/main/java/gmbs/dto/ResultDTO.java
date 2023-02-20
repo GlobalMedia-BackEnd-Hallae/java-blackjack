0698e3d19fdeebe9043d665fca50e37c8de44045
Sy P <yongsa0221@gmail.com>
2023-02-20 22:17:02 +0900

    refactor : result생성 방식 변경

--- src/main/java/gmbs/dto/ResultDTO.java
+++ src/main/java/gmbs/dto/ResultDTO.java
@@ -2,8 +2,11 @@ package gmbs.dto;
 
 import gmbs.model.BlackJackResult;
 import gmbs.model.Card;
+import gmbs.model.black_jack_enum.Result;
 import gmbs.model.players.Player;
 
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class ResultDTO {
@@ -16,9 +19,21 @@ public class ResultDTO {
     public ResultDTO(Player player, BlackJackResult result) {
         this.name = player.getName();
         this.cards = extractCards(player);
-        this.result = result.getResult()
-                .getDescription();
+        this.result = extractResults(result);
         this.cardSum = result.getResultHandSum();
+    }
+
+    private String extractResults(BlackJackResult result) {
+        return result.getResultsData()
+                .entrySet()
+                .stream()
+                .filter(a -> a.getValue() > 0)
+                .map(extractResult())
+                .collect(Collectors.joining());
+    }
+
+    private static Function<Map.Entry<Result, Integer>, String> extractResult() {
+        return a -> a.getValue().toString() + a.getKey().getDescription();
     }
 
     private String extractCards(Player player) {
