5928a80de89d58108b7137d96494bb1096e4eeab
Sy P <yongsa0221@gmail.com>
2023-02-21 04:59:26 +0900

    feat : controller 추가

---
+++ src/main/java/gmbs/controller/Controller.java
@@ -0,0 +1,106 @@
+package gmbs.controller;
+
+import gmbs.dto.DealerDTO;
+import gmbs.dto.ResultDTO;
+import gmbs.dto.UserDTO;
+import gmbs.model.*;
+import gmbs.model.players.Player;
+import gmbs.view.Input;
+import gmbs.view.Output;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Controller {
+    
+    private static final String SPLIT_VALUE = ",";
+    private final Input input = new Input();
+    private final Output output = new Output();
+    private final HandCalculator calculator = new HandCalculator();
+    private BlackJackManager manager;
+
+    public void playBlackJack() {
+        manager = createManager();
+        Player dealer = manager.createDealer();
+        List<Player> users = requestUsers();
+        showFirstStatus(users, dealer);
+        users = usersHit(users);
+        dealer = dealerHit(dealer);
+        WinnerChecker checker = new WinnerChecker(dealer);
+        List<ResultDTO> userResults = getUserResults(checker, users);
+        ResultDTO dealerResult = new ResultDTO(dealer, checker.getTargetResult());
+        showResult(userResults, dealerResult);
+    }
+
+    private BlackJackManager createManager() {
+        Deck deck = new Deck(new CardGenerator().createWholeCards());
+        return new BlackJackManager(deck);
+    }
+
+    private List<Player> requestUsers() {
+        output.startDisplay();
+        return createUserNames()
+                .stream()
+                .map(name -> (Player) manager.createUser(name))
+                .toList();
+    }
+
+    private List<UserName> createUserNames() {
+        return Arrays.stream(requestUserNames())
+                .map(UserName::new)
+                .toList();
+    }
+
+    private String[] requestUserNames() {
+        return input.scan()
+                .split(SPLIT_VALUE);
+    }
+
+    private void showFirstStatus(final List<Player> users, final Player dealer) {
+        output.showPlayerInfo(DealerDTO.oneCardOf(dealer));
+        users.forEach(user -> output.showPlayerInfo(UserDTO.of(user)));
+    }
+
+    private List<Player> usersHit(final List<Player> users) {
+        return users.stream()
+                .map(this::chooseHit)
+                .toList();
+    }
+
+    private Player chooseHit(final Player user) {
+        Player afterHit = user;
+        while (calculator.canHit(afterHit) && isYes(afterHit)) {
+            afterHit = manager.hit(afterHit);
+            output.showPlayerInfo(UserDTO.of(afterHit));
+        }
+        return afterHit;
+    }
+
+    private boolean isYes(final Player user) {
+        output.hitDisplay(user.getName());
+        UserInput userInput = new UserInput(input.scan());
+        return userInput.isYes();
+    }
+
+    private Player dealerHit(final Player dealer) {
+        Player afterHit = dealer;
+        while (calculator.canHit(afterHit)) {
+            afterHit = manager.hit(afterHit);
+            output.showDealerHit();
+        }
+        return afterHit;
+    }
+
+    private List<ResultDTO> getUserResults(final WinnerChecker checker, final List<Player> users) {
+        return users.stream()
+                .map(user -> new ResultDTO(user, checker.getResult(user)))
+                .toList();
+    }
+
+    private void showResult(final List<ResultDTO> users, final ResultDTO dealer) {
+        users.forEach(output::showResultStatus);
+        output.showResultStatus(dealer);
+        users.forEach(output::showResult);
+        output.showResult(dealer);
+    }
+}
