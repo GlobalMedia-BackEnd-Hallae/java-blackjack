c6498b5be55c0231c73ff9f02e9587c992297824
JunSeop Kim <wnqtjq8958@naver.com>
2023-02-21 14:53:30 +0900

    feat : Main 클래스 구현

--- src/main/java/gmbs/Main.java
+++ src/main/java/gmbs/Main.java
@@ -1,7 +1,10 @@
 package gmbs;
 
+import gmbs.game.ConsoleGame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ConsoleGame consoleGame = new ConsoleGame();
+        consoleGame.run();
     }
 }
