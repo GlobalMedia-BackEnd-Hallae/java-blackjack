493493fe9415994770b53aa1ec662e198ea28756
JunSeop Kim <wnqtjq8958@naver.com>
2023-02-22 14:24:08 +0900

    refactor : GameResult 주석 삭제

--- src/main/java/gmbs/domain/game/GameResult.java
+++ src/main/java/gmbs/domain/game/GameResult.java
@@ -36,12 +36,6 @@ public class GameResult {
         return MatchResult.PUSH;
     }
 
-    /**
-     * 파라미터로 주어진 카드 목록 두 개를 비교하고, 첫 번째 카드 목록이 승리할 경우 true 반환.
-     *
-     * @param cards1 - 카드 목록, cards2 - cards1과 비교할 카드 목록
-     * @return cards1이 승리 조건에 부합하면 true
-     */
     private boolean compareCards(Cards cards1, Cards cards2) {
         CardStatus cardStatus1 = cards1.getStatus();
         CardStatus cardStatus2 = cards2.getStatus();
